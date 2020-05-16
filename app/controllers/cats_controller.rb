efcfaefe31958de01a325e0f08692e48acd3e899
J Resilient <j.resilient.1@gmail.com>
2020-05-16 19:59:02 -0400

    Add all matching actions for cats

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,47 @@
+class CatsController < ApplicationController
+    def index
+        render :index
+    end
+
+    def show
+        @current_cat = Cat.find_by(id: params[:id])
+        if @current_cat
+            render :show
+        else
+            render plain: "Could not find cat."
+        end
+    end
+
+    def new
+        render :new
+    end
+
+    def create
+        new_cat = Cat.create(cat_params)
+        if new_cat.save
+            redirect_to cats_url
+        else
+            render json: new_cat.errors.full_messages, status: unprocessable_entity
+        end
+    end
+
+    def edit
+        @cat = Cat.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update
+        cat = Cat.find_by(id: params[:id])
+
+        if cat.update(cat_params)
+            redirect_to cat_url
+        else
+            render json: cat.errors.full_messages
+        end
+    end
+
+    private
+    def cat_params
+        params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+    end
+end
