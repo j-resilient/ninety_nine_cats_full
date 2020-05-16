39bb534a17a97c02a8d241501a7362000c6e88ff
J Resilient <j.resilient.1@gmail.com>
2020-05-16 14:47:52 -0400

    Add #born_previously validation

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -24,18 +24,14 @@ class Cat < ApplicationRecord
     validates :name, uniqueness: true
     validates :sex, inclusion: %w(F M)
     validates :color, inclusion: CAT_COLORS
-    # validate :born_previously
+    validate :born_previously
 
     # private
     def age
         time_ago_in_words(birth_date)
     end
 
-    # def born_previously
-    #     debugger
-    #     # need to check that cat was born before now
-    #     bday = Date.parse(birth_date)
-    #     t = Date.today
-    #     bday <= t
-    # end
+    def born_previously
+        errors[:birth_date] << "must be before today" if (birth_date <=> Date.today) == 1
+    end
 end
