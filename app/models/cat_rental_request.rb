8efe9bf8bb57eb1a7deeb3325557a5115bda0c91
J Resilient <j.resilient.1@gmail.com>
2020-05-18 15:45:57 -0400

    Add #overlapping_requests

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -17,9 +17,18 @@
 class CatRentalRequest < ApplicationRecord
     validates :end_date, :start_date, :cat_id, presence: true
     validates :status, inclusion: %w(PENDING APPROVED DENIED)
+    # validation to make sure all requests are in the future
 
     belongs_to :cat,
         primary_key: :id,
         class_name: :Cat,
         foreign_key: :cat_id
+
+    def overlapping_requests
+        CatRentalRequest
+            .where(cat_id: self.cat_id)
+            .where.not(id: self.id)
+            .where('? > start_date AND end_date > ?', self.end_date, self.start_date)
+
+    end
 end
