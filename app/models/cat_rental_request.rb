97339cec81f503a8df675607233bb6f7048dd4ba
J Resilient <j.resilient.1@gmail.com>
2020-05-18 16:00:03 -0400

    Add #overlapping_approved_requests and #does_not_overlap_approved_request validation

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -17,6 +17,7 @@
 class CatRentalRequest < ApplicationRecord
     validates :end_date, :start_date, :cat_id, presence: true
     validates :status, inclusion: %w(PENDING APPROVED DENIED)
+    validate :does_not_overlap_approved_request
     # validation to make sure all requests are in the future
 
     belongs_to :cat,
@@ -28,7 +29,16 @@ class CatRentalRequest < ApplicationRecord
         CatRentalRequest
             .where(cat_id: self.cat_id)
             .where.not(id: self.id)
-            .where('? > start_date AND end_date > ?', self.end_date, self.start_date)
+            .where('? >= start_date AND end_date >= ?', self.end_date, self.start_date)
+    end
 
+    def overlapping_approved_requests
+        overlapping_requests.where(status: 'APPROVED')
+    end
+
+    def does_not_overlap_approved_request
+        if overlapping_approved_requests.exists?
+            errors[:Cannot] << "request cat during this period, cat is already reserved."
+        end
     end
 end
