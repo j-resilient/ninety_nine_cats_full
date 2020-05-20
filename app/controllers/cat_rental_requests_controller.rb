1e3562cdc6f32de67de3e24136af5d2138ea264c
J Resilient <j.resilient.1@gmail.com>
2020-05-20 15:58:12 -0400

    Add #approve and #deny actions

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -12,6 +12,18 @@ class CatRentalRequestsController < ApplicationController
         end
     end
 
+    def approve
+        request = CatRentalRequest.find_by(id: params[:id])
+        request.approve!
+        redirect_to cat_url(request.cat_id)
+    end
+
+    def deny
+        request = CatRentalRequest.find_by(id: params[:id])
+        request.deny!
+        redirect_to cat_url(request.cat_id)
+    end
+
     private
     def request_params
         params.require(:request).permit(:cat_id, :start_date, :end_date)
