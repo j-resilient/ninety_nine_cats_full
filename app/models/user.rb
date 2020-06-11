8bb6bc257e085a66a9e7b570c36546727730d2a7
J Resilient <j.resilient.1@gmail.com>
2020-06-11 15:47:26 -0400

    Add all methods and validations for user model

---
+++ app/models/user.rb
@@ -0,0 +1,55 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  user_name       :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_session_token  (session_token) UNIQUE
+#  index_users_on_user_name      (user_name) UNIQUE
+#
+class User < ApplicationRecord
+    attr_reader :password
+    after_initialize :ensure_session_token
+
+    validates :user_name, :session_token, presence: true
+    validates :user_name, :session_token, uniqueness: true
+    validates :password_digest, presence: { message: "Password can't be blank"}
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save
+        self.session_token
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(user_name: user_name)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+end
