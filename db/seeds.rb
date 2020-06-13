de95d5faba01df2f0e401a8fca1fb81b1a03f717
J Resilient <j.resilient.1@gmail.com>
2020-06-13 13:56:12 -0400

    Update seeds to generate users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Cat.destroy_all
 CatRentalRequest.destroy_all
+User.destroy_all
 ApplicationRecord.connection.reset_pk_sequence!('cats')
 ApplicationRecord.connection.reset_pk_sequence!('cat_rental_requests')
+ApplicationRecord.connection.reset_pk_sequence!('users')
 
 next_year = Date.today.next_year.year.to_s
 
 10.times do 
+    user = User.create({
+        user_name: Faker::Name.first_name, 
+        password: "password"
+    })
     cat = Cat.create({
         name: Faker::Superhero.name,
         color: Cat::CAT_COLORS.sample,
         sex: %w(F M).sample,
-        birth_date: Faker::Date.birthday(min_age: 1, max_age: 20)
+        birth_date: Faker::Date.birthday(min_age: 1, max_age: 20), 
+        user_id: user.id
     })
 
     CatRentalRequest.create({
