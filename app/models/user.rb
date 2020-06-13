64ed55a0cb5e5aaabbede543ffd7a78836a128c3
J Resilient <j.resilient.1@gmail.com>
2020-06-13 13:58:04 -0400

    Add #cats association

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,11 @@ class User < ApplicationRecord
     validates :password_digest, presence: { message: "Password can't be blank"}
     validates :password, length: { minimum: 6, allow_nil: true }
 
+    has_many :cats, 
+        foreign_key: :user_id,
+        primary_key: :id,
+        class_name: :Cat
+
     def reset_session_token!
         self.session_token = self.class.generate_session_token
         self.save
