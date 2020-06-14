d00a9c354f02482dc3dd8bca5abca0927f34d2ea
J Resilient <j.resilient.1@gmail.com>
2020-06-14 16:38:59 -0400

    Change line order for clarity

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
 Cat.destroy_all
 CatRentalRequest.destroy_all
-User.destroy_all
 ApplicationRecord.connection.reset_pk_sequence!('cats')
 ApplicationRecord.connection.reset_pk_sequence!('cat_rental_requests')
 ApplicationRecord.connection.reset_pk_sequence!('users')
